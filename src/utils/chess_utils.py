21e39f6925e41e3018156c47265a3456b9959fb4
otoobk <otoobk@gmail.com>
2025-06-04 12:02:39 -0400

    Initialized project

---
+++ src/utils/chess_utils.py
@@ -0,0 +1,98 @@
+import pandas as pd
+from datetime import datetime, timezone
+from urllib.parse import unquote, urlparse
+import re
+from utils.file_utils import load_csv, save_csv
+
+"""
+Extract ECO code from PGN
+"""
+def extract_eco_code_from_pgn(pgn):
+    if not pgn:
+        return None
+    match = re.search(r'\[ECO\s+"([A-E][0-9]{2})"\]', pgn)
+    return match.group(1) if match else None
+
+def get_opening_name_from_url(url):
+    if not url:
+        return None
+    if isinstance(url, bytes):
+        url = url.decode("utf-8") 
+    path = urlparse(url).path
+    last_part = path.split("/")[-1]
+    return unquote(last_part.strip().replace("-", " ")).title()
+
+def extract_pgn_datetime(pgn, date_tag="Date", time_tag="StartTime"):
+    date_match = re.search(rf'\[{date_tag} "(\d{{4}}\.\d{{2}}\.\d{{2}})"\]', pgn)
+    time_match = re.search(rf'\[{time_tag} "(\d{{2}}:\d{{2}}:\d{{2}})"\]', pgn)
+    if date_match and time_match:
+        date_str = date_match.group(1).replace('.', '-')
+        time_str = time_match.group(1)
+        try:
+            return datetime.strptime(f"{date_str} {time_str}", "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            return None
+    return None
+
+"""
+Convert Timestamp to Datetime
+"""
+def ts_to_dt(ts):
+            try:
+                return datetime.fromtimestamp(ts, tz=timezone.utc)
+            except Exception:
+                return None
+            
+"""
+Create Pandas DataFrame based on games JSON list
+"""
+def create_games_dataframe(username, games):
+    rows = []
+    for g in games:
+        white = g.get("white", {})
+        black = g.get("black", {})
+        pgn = g.get("pgn", "")
+
+        start = extract_pgn_datetime(pgn, "Date", "StartTime")
+        end = extract_pgn_datetime(pgn, "EndDate", "EndTime")
+        duration = (end - start).total_seconds() if start and end else None
+
+        game_url = g.get("url")
+        game_id = game_url.rstrip("/").split("/")[-1] if game_url else None
+
+        color_played = "white" if white.get("username") == username else "black"
+        result_raw = white.get("result") if color_played == "white" else black.get("result")
+
+        if result_raw == "win":
+            win_loss = "win"
+        elif result_raw in {"checkmated", "timeout", "resigned"}:
+            win_loss = "loss"
+        elif result_raw in {"agreed", "repetition", "stalemate", "insufficient", "50move"}:
+            win_loss = "draw"
+        else:
+            win_loss = "other"
+
+        rows.append({
+            "game_id": game_id,
+            "color_played": color_played,
+            "user_rating": white.get("rating") if color_played == "white" else black.get("rating"),
+            "opponent_rating": white.get("rating") if color_played == "black" else black.get("rating"),
+            "win_loss": win_loss,
+            "victory_method": result_raw,
+            "eco": extract_eco_code_from_pgn(pgn),
+            "date": start.date() if start else None,
+            "duration": duration,
+            "time_class": g.get("time_class"),
+            "rules": g.get("rules"),
+            "opening": get_opening_name_from_url(g.get("eco"))
+        })
+
+    return pd.DataFrame(rows)
+
+"""
+Parse proper Opening name from ECO URL
+"""
+def parse_opening_name(raw_eco):
+    if raw_eco.startswith("http"):
+        return unquote(raw_eco.split("/")[-1]).replace("-", " ").strip().lower()
+    return raw_eco.strip().lower()
---
+++ src/utils/config_utils.py
@@ -0,0 +1,47 @@
+import os
+import yaml
+import argparse
+from pathlib import Path
+
+current_file = os.path.abspath(__file__)
+PROJ_PATH = os.path.dirname(os.path.dirname(os.path.dirname(current_file)))
+CONFIG_PATH = os.path.join(PROJ_PATH, "config.yaml")
+
+"""
+Loads config file containing default configurations
+"""
+def load_config():
+    with open(CONFIG_PATH, "r") as file:
+        return yaml.safe_load(file)
+    
+"""
+Parses command line arguements
+"""
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--username", type=str)
+    parser.add_argument("--allarchives", action="store_true")
+    
+    return parser.parse_args()
+
+"""
+Modified config based on command line arguements
+"""
+def build_final_config(config, args):
+    final_config = config.copy()
+    
+    if args.username:
+        final_config["username"] = args.username
+
+    if args.allarchives:
+        final_config["all_archives"] = "True"
+
+    return final_config
+
+raw_config = load_config()
+args = parse_args()
+
+# Initialize config
+config = build_final_config(raw_config, args)
+
+
