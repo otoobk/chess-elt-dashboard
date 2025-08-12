ca352b826cd2cfd7b1fdd414e6e4d69ff70fa9d4
otoobk <otoobk@gmail.com>
2025-08-12 10:48:00 -0400

    Modified medallian architecture, added SparkSession utils, implemented PySpark DF, write Silver data layer to Parquet

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -21,3 +21,33 @@ def save_csv_with_index(df, path):
 
 def load_csv(path):
     return pd.read_csv(path) if os.path.exists(path) else pd.DataFrame()
+
+def save_csv_sdf(sdf, path, coalesce=True, header=True):
+    out_path = str(path)
+
+    if coalesce:
+        sdf = sdf.coalesce(1) 
+    
+    (
+        sdf.write
+        .option("header", header)
+        .mode("overwrite")
+        .csv(out_path)
+    )
+
+def save_parquet_sdf(sdf, path, coalesce=True, overwrite=True, partition_by=None):
+    out_path = str(path)
+
+    if coalesce:
+        sdf = sdf.coalesce(1)
+
+    writer = (
+                sdf.write
+                    .mode("overwrite" if overwrite else "errorifexists")
+                    .option("compression", "snappy")
+            )
+
+    if partition_by:
+        writer = writer.partitionBy(partition_by)
+
+    writer.parquet(out_path)
